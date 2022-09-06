d42182c9522fc462149cfed508adea3b5dedbe2e
Lucas Duncan <lucasdonuts@gmail.com>
2022-09-06 09:03:50 -0500

    commit tree 4a00c74230ac31e013a7f713d44d1f1a94f5f3e6

---
+++ app/controllers/apartments_controller.rb
@@ -0,0 +1,38 @@
+class ApartmentsController < ApplicationController
+  def index
+    apartments = Apartment.all
+    render json: apartments, status: :ok
+  end
+
+  def show
+    apartment = find_apartment
+    render json: apartment, status: :ok
+  end
+
+  def create
+    apartment = Apartment.create!(apartment_params)
+    render json: apartment, status: :created
+  end
+
+  def update
+    apartment = find_apartment
+    apartment.update!(apartment_params)
+    render json: apartment, status: :ok
+  end
+
+  def destroy
+    apartment = find_apartment
+    apartment.destroy
+    head :no_content
+  end
+
+  private
+
+  def find_apartment
+    Apartment.find(params[:id])
+  end
+
+  def apartment_params
+    params.permit(:number)
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
+
+  private
+
+  def render_not_found_response(error)
+    render json: { errors: error.message }
+  end
+
+  def render_invalid_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }
+  end
 end
